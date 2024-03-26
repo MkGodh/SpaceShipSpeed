581bc0999ce70cfd2480bad459f00b38c0ae46e8
MkGodh <fenryrvla@yandex.ru>
2024-03-26 19:11:54 +0300

    Добавьте файлы проекта.

---
+++ SpaceShipSpeed/SpaceShipSpeed.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <cstring>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+    setlocale(LC_ALL, "RUS");
+    float weight;
+    float tractionForce;
+    float sec;
+    cout << "Введите массу космического коробля: "; cin >> weight;
+    cout << "Введите силу тяги: "; cin >> tractionForce;
+    cout << "Введите на сколько секунд сила тяги: "; cin >> sec;
+
+    float speedSec = tractionForce / weight;
+    float distance = (0.5 * speedSec) * pow(sec, 2);
+
+    cout << "Космолет будет находиться на расстоянии " << distance << " м. от первоначального положения.";
+
+    return 0;
+}
